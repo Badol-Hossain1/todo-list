bac9fee0ad1b55dce6551455e2134ed76b4bd5c5
badolhossain1 <badolhossain586@gmail.com>
2024-05-02 13:58:59 +0600

    fix: fixed the validation

--- app/(components)/schemas/index.tsx
+++ app/(components)/schemas/index.tsx
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const TodoSchema = Yup.object().shape({
-  title: Yup.string().min(2, "Too Short!").max(20, "Too Long!").required(),
+  title: Yup.string().min(2, "Too Short!").max(30, "Too Long!").required(),
   category: Yup.string().min(3, "Too Short!").max(20, "Too Long!").required(),
   description: Yup.string()
     .min(10, "Too Short!")
